5a1c84da76399c0d0d8991d5e2824208979ddb77
KimH85 <61917958+KimH85@users.noreply.github.com>
2020-03-12 16:49:11 -0400

    Updates-2
    Included check for 0 divisor.  Also revamped error handling such that it demonstrates both try/except and conditional check.  Both error types return a descriptive error

--- is_multiple.py
+++ is_multiple.py
@@ -3,13 +3,14 @@ def is_multiple(n, m):
     "Returns True if 'n' is a multiple of 'm'.  Otherwise, returns False."
     
     #check to be sure parameters are integers
-    if isinstance(n, int) == False or isinstance(m, int) == False:
-        print("Invalid parameter passed in.  These must be integers.")
-        
-        
-    else:
-        
-        #if 0 remainder when dividing, then this is a multiple and returns True.
-        #else if not 0 remainder when dividing, then this is not a multiple and returns False.
-        return n % m == 0
+    if isinstance(n, int) == True and isinstance(m, int) == True:
+        try:
+            return n % m == 0  # result 0 means 'n' is multiple of 'm' - returns True.  Otherwise returns False
+         
+        #if m is 0 - this will cause mathematical error - show message
+        except ZeroDivisionError:
+            print("Invalid 'm' parameter passed in.  Cannot be 0.")      
     
+    #parameters were not integers - show message
+    else:
+        print("Invalid parameter(s) passed in.  They must both be integers.")  
